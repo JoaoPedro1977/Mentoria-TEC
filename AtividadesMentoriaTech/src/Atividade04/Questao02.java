ae5f844ec9af5a642ebf6df9590506cef867bfad
João Pedro <joao.vasconcelos.2009@aluno.edu.pi.gov.br>
2025-06-04 10:00:58 -0300

    Meu segundo codigo Java

---
+++ AtividadesMentoriaTech/src/Atividade04/Questao02.java
@@ -0,0 +1,12 @@
+package Atividade04;
+
+public class Questao02 {
+    public static void main(String[] args) {
+        int idade1 = 20;
+        int idade2 = 12;
+
+        boolean resultado = idade1 > idade2;
+        System.out.println("a primeira pessoa é mais velha: " + resultado);
+
+    }
+}
