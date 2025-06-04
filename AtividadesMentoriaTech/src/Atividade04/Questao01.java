296659503436df4990e99312786dcf621c96efd4
João Pedro <joao.vasconcelos.2009@aluno.edu.pi.gov.br>
2025-06-04 09:40:18 -0300

    meu codigo java

---
+++ AtividadesMentoriaTech/src/Atividade04/Questao01.java
@@ -0,0 +1,14 @@
+package Atividade04;
+
+public class Questao01 {
+    public static void main(String[] args) {
+        double base = 5;
+        double altura = 9;
+
+        double area = base * altura;
+        double perimetro = 2 * (base + altura);
+
+        System.out.println("Area do retangulo: " + area);
+        System.out.println("Perimetro do retangulo: " + perimetro);
+    }
+}
